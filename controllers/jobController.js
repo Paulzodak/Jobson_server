c989322bc835c0eca19a7fd3d78035038b2a5d28
Zodak <ojepaul4jesus@gmail.com>
2023-05-23 13:56:04 +0100

    fix: puppeteer fix 2

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,13 +1,13 @@
 import puppeteer from "puppeteer";
 export const getJobDetails = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Max-Age", "1800");
+  // res.setHeader("Access-Control-Allow-Headers", "content-type");
+  // res.setHeader(
+  //   "Access-Control-Allow-Methods",
+  //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  // );
   async function run() {
     const browser = await puppeteer.launch({
       headless: "new",
