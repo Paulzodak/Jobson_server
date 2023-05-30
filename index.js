875b2b8aa54b22572ad01a79904cbd81f04c625d
Zodak <ojepaul4jesus@gmail.com>
2023-05-30 12:11:50 +0100

    chore : added delete all notifications

--- index.js
+++ index.js
@@ -49,19 +49,84 @@ mongoose
   )
   .then(() =>
     server.listen(PORT, () => {
-      console.log("started");
+      // console.log("started");
+
+      // -------NEEDS REFACTORING-----
+      User.watch().on("change", async (data) => {
+        // console.log(data);
+        const { operationType, fullDocument } = data;
+        const user = await User.findOne({
+          _id: data.documentKey._id,
+        });
+        if (operationType === "insert") {
+          const notification = new Notification({
+            id: user._id.toString(),
+            title: `Welcome ${user.fullname} `,
+            seen: false,
+            message: `Welcome to Jobson ${user.fullname}, Your open source platform for job hunting.`,
+          });
+          try {
+            await notification.save();
+          } catch (error) {
+            // console.log(error);
+          }
+          const notification2 = new Notification({
+            id: user._id.toString(),
+            title: `Next step ${user.fullname}`,
+            seen: false,
+            message: `We appreciate your choice for using our platform, to make it easier for us to generate appropriate jobs for you, proceed to the profile page and fill in your information, Thank you.`,
+          });
+          try {
+            await notification2.save();
+          } catch (error) {
+            // console.log(error);
+          }
+        }
+        if (operationType === "update") {
+          // const { _id, email, imageUrl } = fullDocument;
+
+          // console.log(user);
+          // Notification.aggregate([
+          //   // Match documents with the given ID
+          //   { $match: { id: user._id } },
+          //   // Sort the matched documents by the "createdAt" field in descending order
+          //   { $sort: { createdAt: -1 } },
+          // ]);
+          if (
+            !user.cv ||
+            !user.imageUrl ||
+            !user.experienceYears ||
+            !user.stack ||
+            !user.bio
+          ) {
+            // console.log("updatee profile");
+            const notification = new Notification({
+              id: user._id.toString(),
+              title: "Please update your profile!",
+              seen: false,
+              message:
+                "Please update your profile to allow our engines generate more accurate job listing",
+            });
+            try {
+              await notification.save();
+            } catch (error) {
+              // console.log(error);
+            }
+          }
+        }
+      });
     })
   )
   .catch((err) => {});
 
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+  // console.log(`User Connected: ${socket.id}`);
 
   socket.on("connect_error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
+    // console.log(`connect_error due to ${err.message}`);
   });
   socket.on("connectNotification", async (id) => {
-    console.log(id);
+    // console.log(id);
     // socket.userid = id;
 
     try {
@@ -71,7 +136,7 @@ io.on("connection", (socket) => {
         // Sort the matched documents by the "createdAt" field in descending order
         { $sort: { createdAt: -1 } },
       ]);
-      console.log("notifications" + result);
+      // console.log("notifications" + result);
       socket.emit("sendNotificationResult", result);
     } catch (error) {
       socket.emit("sendNotificationResult", "error");
@@ -79,7 +144,7 @@ io.on("connection", (socket) => {
 
     Notification.watch().on("change", async () => {
       // const { operationType, fullDocument } = data;
-      console.log("new change");
+      // console.log("new change");
 
       try {
         const result = await Notification.aggregate([
@@ -88,7 +153,7 @@ io.on("connection", (socket) => {
           // Sort the matched documents by the "createdAt" field in descending order
           { $sort: { createdAt: -1 } },
         ]);
-        console.log("newchanges" + result);
+        // console.log("newchanges" + result);
 
         socket.emit("sendNotificationResult", result);
       } catch (error) {
@@ -111,51 +176,36 @@ io.on("connection", (socket) => {
       );
     } catch (error) {}
   });
+  socket.on("updateAllNotification", async (id) => {
+    // console.log("i ran");
+    try {
+      await Notification.updateMany(
+        {
+          id: id,
+        },
+        {
+          $set: {
+            seen: true,
+          },
+        }
+      );
+    } catch (error) {}
+  });
   socket.on("deleteNotification", async (id) => {
     try {
       await Notification.deleteOne({ _id: id });
     } catch (error) {}
   });
+  socket.on("deleteAllNotification", async (id) => {
+    // console.log("i ran");
+    try {
+      await Notification.deleteMany({
+        id: id,
+      });
+    } catch (error) {}
+  });
 
   socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
+    // console.log("User Disconnected", socket.id);
   });
-});
-
-User.watch().on("change", async (data) => {
-  console.log(data);
-  const { operationType, fullDocument } = data;
-  if (operationType === "insert" || operationType === "update") {
-    // const { _id, email, imageUrl } = fullDocument;
-    const user = await User.findOne({
-      _id: data.documentKey._id,
-    });
-    console.log(user);
-    Notification.aggregate([
-      // Match documents with the given ID
-      { $match: { id: user._id } },
-      // Sort the matched documents by the "createdAt" field in descending order
-      { $sort: { createdAt: -1 } },
-    ]);
-    if (
-      !user.cv ||
-      !user.imageUrl ||
-      !user.experienceYears ||
-      !user.stack ||
-      !user.bio
-    ) {
-      const notification = new Notification({
-        id: user._id,
-        title: "Please update your profile!",
-        seen: false,
-        message:
-          "Please update your profile to allow our engines generate more accurate job listing",
-      });
-      try {
-        await notification.save();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
 });
